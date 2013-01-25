dbdbf374096c9c62e78e427b0fe5283f1ac9aa51
Brent Brimhall <brent.brimhall@gmail.com>
2013-01-24 17:18:44 -0700

    Shell script to clean and update from git.

---
+++ update_from_git.sh
@@ -0,0 +1,4 @@
+git reset --hard # removes staged and working directory changes
+git clean -f -d # remove untracked files
+git pull
+# git clean -f -x -d # CAUTION: as above but removes ignored files like config.
